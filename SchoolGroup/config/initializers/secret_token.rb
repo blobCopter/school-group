1b9afddad7111a78c1c4862c66e02681b79faf24
Patrick Rabier <patrick.rabier@gmail.com>
2013-05-18 17:27:09 +0800

    First commit

---
+++ SchoolGroup/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchoolGroup::Application.config.secret_token = 'd84b87e0be93e38703a5c0c60723a13f479cacb3675b5d411c490dff892d3b9435514c4925a069390c16fdffbc2d338ecd097b9e20c9f56b7bad5bcae6fdf15c'
