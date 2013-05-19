4b4059cf82e8078a705c83ad484e6e1fefc7d0c6
Patrick Rabier <patrick.rabier@gmail.com>
2013-05-19 13:28:04 +0800

    new check boxes

--- SchoolGroup/app/assets/javascripts/group.js
+++ SchoolGroup/app/assets/javascripts/group.js
@@ -23,13 +23,71 @@
 	    		$(this).removeAttr("class");
 			});
 	    });
-	    $('tr input[type=checkbox]').click(function (e) {
-	    	var current_state = ($( "tr input:checked" ).length / $('tr input[type=checkbox]').length) * 100;
-	    	$('#current-state').width(current_state + "%");
-	    });
+
 	
-	$('tr input[type=checkbox]').click(function (e) {
+	$('tr input[type=checkbox]').on('ifChecked', function(event){
+	    var task_id = $(this).attr("task_id");
+	    var isChecked = $(this).prop("checked")  ? 1 : 0;
+
+	    var current_state = ($( "tr input:checked" ).length / $('tr input[type=checkbox]').length) * 100;
+	    $('#current-state').width(current_state + "%");
+
+	    $.ajax({
+		
+			type: "POST",
+			
+			url: "http://localhost:3000/users/topics/1/group/1/toggle_status.json",
+
+			dataType: 'text',
+			
+			data: {task_id: task_id, status: isChecked},
+			
+			success: function(data) {
+			  //   	<div class="alert alert-success" style="display:none;"></div>
+					// <div class="alert alert-error" style="display:none;"></div>
+			    $('#task_list .alert-success').show(0).delay(5000).hide(0);
+			},
+			
+			error: function(data) {
+			    
+			    console.log(data);
+			    
+			}
+	    });
+	});
+		$('tr input[type=checkbox]').on('ifUnchecked', function(event){
+	    var task_id = $(this).attr("task_id");
+	    var isChecked = $(this).prop("checked")  ? 1 : 0;
+
+	    var current_state = ($( "tr input:checked" ).length / $('tr input[type=checkbox]').length) * 100;
+	    $('#current-state').width(current_state + "%");
 	    
+	    $.ajax({
+		
+			type: "POST",
+			
+			url: "http://localhost:3000/users/topics/1/group/1/toggle_status.json",
+
+			dataType: 'text',
+			
+			data: {task_id: task_id, status: isChecked},
+			
+			success: function(data) {
+			  //   	<div class="alert alert-success" style="display:none;"></div>
+					// <div class="alert alert-error" style="display:none;"></div>
+			    $('#task_list .alert-success').show(0).delay(5000).hide(0);
+			},
+			
+			error: function(data) {
+			    
+			    console.log(data);
+			    
+			}
+	    });
+	});
+	/*$('tr input[type=checkbox]').click(function (e) {
+	    
+	    console.log("event");
 	    var task_id = $(this).attr("task_id");
 	    var isChecked = $(this).prop("checked")  ? 1 : 0;
 	    $.ajax({
@@ -54,7 +112,7 @@
 			    
 			}
 	    });
-	});
+	});*/
 	$('#task_list').ready(function() {
 		var current_state = ($( "tr input:checked" ).length / $('tr input[type=checkbox]').length) * 100;
 	    $('#current-state').width(current_state + "%");
--- SchoolGroup/app/controllers/users_controller.rb
+++ SchoolGroup/app/controllers/users_controller.rb
@@ -1,18 +1,43 @@
 class UsersController < ApplicationController
   def group
     @group = Group.find(params[:id])
+    @topic = Topic.find(@group.topic_id)
     @users = @group.users
     @tasks = @group.tasks
+    tasks = Task.where("group_id='#{@group.id}'")
+    	total =  tasks.count
+    	validated_tasks =  tasks.where("status='1'").count
+		if (total > 0)
+    		@completion = ((validated_tasks.to_f / total.to_f) * 100.0)
+    	else
+    		@completions = 0
+    	end
     respond_to do |format|
       format.html
     end
   end
 
   def topic
+  	@completions = []
     @user = User.find(params[:id])
  	@users = User.all
     @topic = @user.topics.first
     @groups = @topic.groups
+    @groups.each do |group|
+    	tasks = Task.where("group_id='#{group.id}'")
+    	total =  tasks.count
+    	validated_tasks =  tasks.where("status='1'").count
+		if (total > 0)
+    		@completions << ((validated_tasks.to_f / total.to_f) * 100.0)
+    	else
+    		@completions << 0
+    	end
+    	puts "----------------------------------"
+    	puts total
+    	puts validated_tasks
+    	puts((validated_tasks.to_f / total.to_f) * 100.0)
+    	puts "----------------------------------"
+    end	
     @list = @topic.list
     respond_to do |format|
       format.html
